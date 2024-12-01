1f2e0020f6303589928d5fb535b9d865e899eaf3
DarioN370 <netobis2015@gmail.com>
2024-12-01 00:17:52 -0300

    update files

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@
 nome = input ('Qual o seu nome?')
 dia = input('Que dia você nasceu?')
 mes = input('Qual mês ?')
-ano = int(input('Qual ano ?'))  #usei o int para mostrar que o input seria um numero inteiro para que eu pudesse montar a conta de subtração na linha de baixo
+ano = int(input('Qual ano ?'))  # usei o int para mostrar que o input seria um numero inteiro para que eu pudesse montar a conta de subtração na linha de baixo
 idade = 2024 - ano
 
 print('sendo assim', nome , ', voce tem' , idade, 'anos,', 'pois nasceu dia', dia, 'do mês de', mes, 'do ano de', ano)
